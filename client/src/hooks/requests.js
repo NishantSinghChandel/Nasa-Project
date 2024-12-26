48c16e537f117ed1f7ff86aa3526dce1b2e19acb
Nishant Singh <realnishantsingh@gmail.com>
2024-12-26 19:51:14 +0530

    add create, delete launch

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8000';
+const API_URL = "http://localhost:8000";
 
 async function httpGetPlanets() {
   const response = await fetch(`${API_URL}/planets`);
@@ -9,23 +9,32 @@ async function httpGetPlanets() {
 async function httpGetLaunches() {
   // TODO: Once API is ready.
   // Load launches, sort by flight number, and return as JSON.
-  const response = await fetch(`${API_URL}/launches`)
-  return await response.json()
+  const response = await fetch(`${API_URL}/launches`);
+  return await response.json();
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      body: JSON.stringify(launch),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {
+    return { ok: false };
+  }
 }
 
 async function httpAbortLaunch(id) {
-  // TODO: Once API is ready.
-  // Delete launch with given ID.
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch (err) {
+    return { ok: false };
+  }
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,16 +1,51 @@
 const launches = new Map();
+
+let latestFlightNumber = 100;
+
 const launch = {
   flightNumber: 100,
   mission: "Kepler Expolaration X",
   rocket: "Gaganyan",
   launchDate: new Date("December 27, 2030"),
-  destination: "Kepler-442 b",
+  target: "Kepler-442 b",
   customer: ["SpaceX", "NASA"],
   upcoming: true,
   success: true,
 };
 
 launches.set(launch.flightNumber, launch);
-module.exports = {
-        launches
+
+function getAllLaunches() {
+  return Array.from(launches.values());
 }
+
+function existsLaunchWithId(launchId) {
+  return launches.has(launchId);
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      flightNumber: latestFlightNumber,
+      upcoming: true,
+      customers: ["FWC"],
+      success: true,
+    })
+  );
+}
+
+function abortLaunchById(launchId) {
+  const aborted = launches.get(launchId);
+  aborted.upcoming = false;
+  aborted.success = false;
+  return aborted;
+}
+
+module.exports = {
+  getAllLaunches,
+  addNewLaunch,
+  existsLaunchWithId,
+  abortLaunchById
+};
